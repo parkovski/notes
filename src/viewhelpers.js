422b49603c68f67d2c5b2d629681eee72492502a
Parker Snell <parkersnell@gmail.com>
2014-02-26 14:00:51 -0700

    moz-focus-inner

--- src/viewhelpers.js
+++ src/viewhelpers.js
@@ -33,6 +33,7 @@ var _writeCss = function(where) {
 
 // Blocking apis allowed - see compileLessFiles.
 var compileThemes = function(themebase, styleDir) {
+  console.log('compiling all themes from ', themebase);
   var themevars = (function() {
     // The less compiler translates --modify-var to a piece of code and tacks it
     // on to the file, so we'll do the same thing.
@@ -58,6 +59,7 @@ var compileThemes = function(themebase, styleDir) {
 
 // Blocking apis allowed - see compileLessFiles.
 var compileLess = function(lessFile, cssFile) {
+  console.log('compiling ', lessFile);
   less.render('' + fs.readFileSync(lessFile), _writeCss(cssFile));
 };
 
@@ -65,19 +67,19 @@ var compileLess = function(lessFile, cssFile) {
 module.exports.compileLessFiles = function compileLessFiles() {
   var styleDir = __dirname + '/../style/';
   fs.readdirSync(styleDir).forEach(function(file) {
-    if (/\.css$/.test(file)) return;
+    if (!/\.less$/.test(file)) return;
     var fullLess = styleDir + file;
     var css = file.replace(/\.less$/, '.css');
-    var fullCss = styleDir + css;
     var isThemebase = false;
     if (file === 'themebase.less') {
       // check one to see if they should be recompiled.
       css = 'theme0.css';
       isThemebase = true;
     }
+    var fullCss = styleDir + css;
     var lessTime = fs.statSync(fullLess).mtime;
     var cssTime = fs.existsSync(fullCss)
-      ? fs.statSync(fullCss)
+      ? fs.statSync(fullCss).mtime
       : 0;
 
     if (lessTime > cssTime) {
