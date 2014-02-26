5766f0e758579b21571c216921700452efe700cb
Parker Snell <parkersnell@gmail.com>
2014-02-25 17:53:09 -0700

    don't need to compile themes anymore!

--- src/viewhelpers.js
+++ src/viewhelpers.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var Handlebars = require('handlebars');
+var less = require('less');
 
 module.exports.setupAppForHandlebars = function setupAppForHandlebars(app) {
   app.set('views', __dirname + '/views');
@@ -18,37 +19,78 @@ module.exports.registerPartials = function registerPartials() {
   });
 };
 
-var lessfiles = {};
-var renderLess = function(name, res) {
-  if (name in lessfiles) {
-    res.end(lessfiles[name]);
-  } else {
-    var rendername = name;
-    if (/theme.+\.less/.match(name)) {
-      rendername = 'themebase.less';
-      // TODO: fill in theme vars
-    }
-    fs.read(__dirname + '/../style/' + name, function(err, contents) {
-      if (err) {
-        lessfiles[name] = contents = '/* file read error */';
-      }
-      less.render(contents, function(err, css) {
-        if (err) {
-          lessfiles[name] = css = '/* less render error */';
-        }
-        res.end(css);
-      });
+var _csscache = {};
+var _themevars = (function() {
+  // The less compiler translates --modify-var to a piece of code and tacks it
+  // on to the file, so we'll do the same thing.
+  var themevars = require('./themes.json');
+  var themecode = {};
+  Object.keys(themevars).forEach(function(theme) {
+    var code = '';
+    Object.keys(themevars[theme]).forEach(function(v) {
+      code += '\n@' + v + ': ' + themevars[theme][v] + ';';
     });
+    themecode[theme] = code;
+  });
+  return themecode;
+})();
+var renderLess = function(name, res) {
+  var rendername;
+  var themeid = null;
+  var match = /theme(.+)\.css$/.exec(name);
+  if (match) {
+    rendername = __dirname + '/../style/themebase.less';
+    themeid = match[1];
+  } else {
+    rendername = __dirname + '/../style/' + name.replace(/\.css$/, '.less');
   }
+  fs.readFile(rendername, function(err, contents) {
+    if (err) {
+      res.writeHeader(404);
+      res.end();
+      return;
+    }
+    contents = '' + contents;
+    if (themeid !== null) {
+      contents += _themevars[themeid];
+    }
+    less.render(contents, function(err, css) {
+      if (err) {
+        console.log('*** less render error');
+        console.log(err);
+        css = '/* less render error */';
+      }
+      _csscache[name] = css;
+      res.end(css);
+    });
+  });
 };
 
+// When we're asked for style/x.css, do these things:
+// 1) If x.css is cached, serve that.
+// 2) If x.css exists on disk, cache and serve it.
+// 3) If x is theme[n].css, compile themebase.less for the appropriate theme;
+//    cache and serve that.
+// 4) If x.less exists on disk, compile, cache and serve it.
+// 5) 404.
 module.exports.getStyleResponseFunction = function() {
+  // hacky check to see if themes were compiled or not. probably remove this.
   if (fs.existsSync(__dirname + '/../style/style0.css')) {
     return require('express').static(__dirname + '/../style');
   }
-  var less = require('less');
+
   return function(req, res) {
-    var name = req.url.substring(1).replace(/\.css$/, '.less');
-    renderLess(name, res);
+    var cssname = req.url.substring(1);
+    var diskFileName = __dirname + '/../style/' + cssname;
+    res.setHeader('Content-Type', 'text/css');
+    if (_csscache.hasOwnProperty(cssname)) {
+      return res.end(_csscache[cssname]);
+    }
+    fs.readFile(diskFileName, function(err, contents) {
+      if (err) {
+        return renderLess(cssname, res);
+      }
+      res.end(_csscache[cssname] = contents);
+    });
   };
 };
