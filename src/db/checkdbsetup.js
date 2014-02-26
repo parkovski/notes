855d8c7893e2778812d55fe7650e76dbda7ec834
Parker Snell <parkersnell@gmail.com>
2014-02-25 21:04:58 -0700

    db setup

--- src/db/checkdbsetup.js
+++ src/db/checkdbsetup.js
@@ -1,13 +1,16 @@
 var db = require('./db');
 
-var q = function(query) {
-  db.query('CREATE TABLE IF NOT EXISTS ' + query);
+var makeSureTableExists = function(query) {
+  db.query('CREATE TABLE IF NOT EXISTS ' + query + ';');
 }
 
 module.exports = function() {
-  /*
   [
-    ''
-  ].map(q);
-  */
+    'users (id serial PRIMARY KEY,'
+      + ' name varchar(32),'
+      + ' password binary(20),'
+      + ' email varchar(48),'
+      + ' theme tinyint DEFAULT 0)'
+      + ' ENGINE=InnoDB DEFAULT CHARSET=utf8'
+  ].map(makeSureTableExists);
 };
