db5f84e54d3bc7a69cd011e2b69b5f373bc640dc
Parker Snell <parkersnell@gmail.com>
2014-04-01 17:04:11 -0700

    db error messages

--- src/db/db.js
+++ src/db/db.js
@@ -15,17 +15,17 @@ function handleDisconnect() {
 
   connection.connect(function(err) {
     if(err) {
-      console.log('error when connecting to db:', err);
+      console.error(err);
       throw err;
       //setTimeout(handleDisconnect, 2000);
     }
   });
 
   connection.on('error', function(err) {
-    console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') {
       handleDisconnect();
     } else {
+      console.error(err);
       throw err;
     }
   });
