6b7b1661f95372481c371eef58d308ed32227fde
Parker Snell <parkersnell@gmail.com>
2014-05-20 13:57:30 -0700

    sidebar menu

--- src/clientjs/dropdownmenu.js
+++ src/clientjs/dropdownmenu.js
@@ -1,39 +1,12 @@
 require(['jquery'], function($) {
   'use strict';
   
-  function open(menu, button) {
-    menu.css({display: 'block'});
-    button.addClass('sel');
-  }
-
-  function close(menu, button) {
-    menu.css({display: 'none'});
-    button.removeClass('sel');
-  }
-
-  function dropdownMenu(toggle, div) {
-    if (!(toggle instanceof $)) toggle = $('#' + toggle);
-    if (!(div instanceof $)) div = $('#' + div);
-    toggle.click(function(e) {
-      var disp = div.css('display');
-      if (disp === 'none') {
-        open(div, toggle);
-      } else {
-        close(div, toggle);
-      }
-      e.stopPropagation();
-      return false;
-    });
-  };
-
   $(function() {
-    $('.dropdown').each(function() {
-      var $this = $(this);
-      dropdownMenu($this.data('toggle'), $this);
-    });
-    $(document).click(function() {
-      $('.dropdown').css({display: 'none'});
-      $('.dropdownButton').removeClass('sel');
+    var $sidebar = $('#sidebar');
+    var $main = $('#main');
+    $('#toggleSidebar').click(function() {
+      $main.toggleClass('out');
+      $sidebar.toggleClass('out');
     });
   });
 });
