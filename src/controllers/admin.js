25083cccd07a2d848b23f42c5525d6c6cd9a031c
Parker Snell <parkersnell@gmail.com>
2014-05-21 15:40:28 -0700

    admin restarting but not really
    this provides the frontend for a restart button, but doesn't actually
    implement it. not really sure why i did this.

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,8 +1,12 @@
 'use strict';
 
+var fs = require('fs');
+
 var adminModel = require('../models/admin');
 
 function AdminController(configure) {
+  var self = this;
+  
   configure('*').before(function(req, res, next) {
     if (!adminModel.isUserAdmin(req.user)) {
       return res.redirect('/');
@@ -10,16 +14,28 @@ function AdminController(configure) {
     
     next();
   });
+  
+  self.canRestart = false;
+  fs.stat(__dirname + '/../../restart.sh', function(err, stats) {
+    self.canRestart = !err && stats && stats.isFile();
+  });
 }
 
 AdminController.prototype.showHomePage = function(req, res) {
+  var self = this;
+  
   adminModel.getUserCount(function(err, users) {
     if (err) console.error(err);
     res.render('admin/index.html', {
       title: 'Admin',
-      numUsers: users
+      numUsers: users,
+      canRestart: self.canRestart
     });
   });
+};
+
+AdminController.prototype.restart = function(req, res) {
+  res.end('not supported');
 };
 
 module.exports = AdminController;
