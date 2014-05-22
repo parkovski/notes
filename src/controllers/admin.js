446ab0ed45b5d593b67f53bc1e251099b56aabbb
Parker Snell <parkersnell@gmail.com>
2014-05-22 12:32:38 -0700

    crude self updater

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var fs = require('fs');
+var child_process = require('child_process');
+var path = require('path');
 
 var adminModel = require('../models/admin');
 
@@ -14,28 +16,56 @@ function AdminController(configure) {
     
     next();
   });
-  
-  self.canRestart = false;
-  fs.stat(__dirname + '/../../restart.sh', function(err, stats) {
-    self.canRestart = !err && stats && stats.isFile();
-  });
 }
 
 AdminController.prototype.showHomePage = function(req, res) {
   var self = this;
+  
+  var time = Math.floor(process.uptime());
+  var uptime = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    totalSeconds: time
+  };
+  uptime.seconds = time % 60; time = Math.floor(time / 60);
+  uptime.minutes = time % 60; time = Math.floor(time / 60);
+  uptime.hours = time % 24; time = Math.floor(time / 24);
+  uptime.days = time;
   
   adminModel.getUserCount(function(err, users) {
     if (err) console.error(err);
     res.render('admin/index.html', {
       title: 'Admin',
       numUsers: users,
-      canRestart: self.canRestart
+      uptime: uptime
     });
   });
 };
 
-AdminController.prototype.restart = function(req, res) {
-  res.end('not supported');
+AdminController.prototype.gitPull = function(req, res) {
+  var gitCwd = path.normalize(__dirname + '/../..');
+  child_process.exec('git pull', { cwd: gitCwd }, function(err, stdout, stderr) {
+    if (err) console.error(err);
+    res.render('admin/output.html', {
+      title: 'Git pull',
+      sections: [{ name: 'Admin', url: '/admin' }],
+      stdout: stdout,
+      stderr: stderr
+    });
+  });
+};
+
+AdminController.prototype.crash = function(req, res) {
+  res.render('admin/output.html', {
+    title: 'Crash',
+    sections: [{ name: 'Admin', url: '/admin' }],
+    stdout: 'Wait a minute, then go back to admin. Hope you didn\'t break anything.',
+    stderr: 'Good luck!'
+  });
+  
+  setTimeout(process.exit, 150);
 };
 
 module.exports = AdminController;
